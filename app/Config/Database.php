3f92eec1eff49d872a4553118e1cbedf4d83911a
mikweb <ijtihah6@gmail.com>
2017-06-19 23:40:42 +0000

    Update Database.php
    Formating

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Database configuration
  *
@@ -6,41 +7,37 @@
  * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
  * @version 3.0
  */
-
-
 return array(
-    // The PDO Fetch Style.
-    'fetch' => PDO::FETCH_CLASS,
-
-    // The Default Database Connection Name.
-    'default' => 'mysql',
-
-    // The Database Connections.
-    'connections' => array(
-        'sqlite' => array(
-            'driver'    => 'sqlite',
-            'database'  => APPDIR .'Storage' .DS .'database.sqlite',
-            'prefix'    => '',
-        ),
-        'mysql' => array(
-            'driver'    => 'mysql',
-            'hostname'  => 'localhost',
-            'database'  => 'nova',
-            'username'  => 'nova',
-            'password'  => 'password',
-            'prefix'    => PREFIX,
-            'charset'   => 'utf8',
-            'collation' => 'utf8_general_ci',
-        ),
-        'pgsql' => array(
-            'driver'   => 'pgsql',
-            'host'     => 'localhost',
-            'database' => 'nova',
-            'username' => 'nova',
-            'password' => 'password',
-            'charset'  => 'utf8',
-            'prefix'   => PREFIX,
-            'schema'   => 'public',
-        ),
-    ),
+   // The PDO Fetch Style.
+   'fetch' => PDO::FETCH_CLASS,
+   // The Default Database Connection Name.
+   'default' => 'mysql',
+   // The Database Connections.
+   'connections' => array(
+      'sqlite' => array(
+         'driver' => 'sqlite',
+         'database' => APPDIR . 'Storage' . DS . 'database.sqlite',
+         'prefix' => '',
+      ),
+      'mysql' => array(
+         'driver' => 'mysql',
+         'hostname' => 'localhost',
+         'database' => 'c0rat',
+         'username' => 'c0rat',
+         'password' => 'gjyW4N#B',
+         'prefix' => PREFIX,
+         'charset' => 'utf8',
+         'collation' => 'utf8_general_ci',
+      ),
+      'pgsql' => array(
+         'driver' => 'pgsql',
+         'host' => 'localhost',
+         'database'  => 'nova',
+         'username'  => 'nova',
+         'password'  => 'password',
+         'charset' => 'utf8',
+         'prefix' => PREFIX,
+         'schema' => 'public',
+      ),
+   ),
 );
