1de2922c7a9e6845098fd7ef04df79859a436c04
mikweb <ijtihah6@gmail.com>
2017-06-20 00:34:52 +0000

    Update Bootstrap.php
    handle absolute path to 'assets' folder

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -6,3 +6,11 @@
  * @version 3.0
  */
 
+// Handle absolute path to 'assets' folder (FileField)
+if (!function_exists('assets_url')) {
+
+   function assets_url($path) {
+      return resource_url(str_replace(ROOTDIR . 'assets/', '', $path));
+   }
+
+}
