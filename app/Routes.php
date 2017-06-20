c55a5ee6771e3d9cbd78a447fe9a20bc9fd4fec2
mikweb <ijtihah6@gmail.com>
2017-06-20 00:40:27 +0000

    Update Routes.php
    Clean application routes.

--- app/Routes.php
+++ app/Routes.php
@@ -1,39 +1,24 @@
 <?php
+
 /**
  * Routes - all standard Routes are defined here.
  *
  * @author David Carr - dave@daveismyname.com
  * @version 3.0
  */
-
-
 /** Define static routes. */
-
-// Default Routing
-Route::any('', function() {
-    $content = __('Yep! It works.');
-
-    $view = View::make('Default')
-        ->shares('title', __('Welcome'))
-        ->withContent($content);
-
-    return View::makeLayout('Welcome')->withContent($view);
-});
-
 // The Language Changer
-Route::any('language/{language}', array('before' => 'referer', function($language)
-{
-    $languages = Config::get('languages');
+Route::any('language/{language}', array('before' => 'referer', function($language) {
+      $languages = Config::get('languages');
 
-    if (in_array($language, array_keys($languages))) {
-        Session::set('language', $language);
+      if (in_array($language, array_keys($languages))) {
+         Session::set('language', $language);
 
-        // Store also the current Language in a Cookie lasting five years.
-        Cookie::queue(PREFIX .'language', $language, Cookie::FIVEYEARS);
-    }
+         // Store also the current Language in a Cookie lasting five years.
+         Cookie::queue(PREFIX . 'language', $language, Cookie::FIVEYEARS);
+      }
 
-    return Redirect::back();
-
-}))->where('language', '([a-z]{2})');
+      return Redirect::back();
+   }))->where('language', '([a-z]{2})');
 
 /** End default Routes */
